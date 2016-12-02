afdfa774dbcffc36691234c06da9445b54ed59d5
FireJohnny <FireJohnny@outlook.com>
2016-12-02 11:03:41 +0800

    loadtestSet

--- project.py
+++ project.py
@@ -1,10 +1,22 @@
 #coding:utf-8
 __author__ = 'FireJohnny'
 
-from numpy import *
+import numpy as np
 from matplotlib import *
 
-def loadFile():
-    dataMat = []
-    LabelMat = []
+def loadFile(fileName):
+    dataArr = []
+    LabelArr = []
+    with open(fileName) as fr:
+        for line in fr.readlines():
+            lineArr = line.strip().split()
+            #print lineArr
+            dataArr.append([1.0,float(lineArr[0]),float(lineArr[1])])
+            LabelArr.append(int(lineArr[2]))
+
+    return dataArr,LabelArr
+
+dataArr,LabelArr = loadFile("testSet.txt")
+print dataArr,"\n",LabelArr
+
     
