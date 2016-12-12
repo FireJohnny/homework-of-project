8b660e300e90134cfb568e7028333a7bb6ecc55e
FireJohnny <FireJohnny@outlook.com>
2016-12-12 19:53:27 +0800

    no

--- project.py
+++ project.py
@@ -2,25 +2,34 @@
 __author__ = 'FireJohnny'
 
 import numpy as np
-from matplotlib import *
+import matplotlib.pyplot as plt
 
 def loadFile(fileName):
-    dataArr = []
-    LabelArr = []
+    dataMat = []
+    LabelMat = []
+
     with open(fileName) as fr:
         for line in fr.readlines():
             lineArr = line.strip().split()
             #print lineArr
-            dataArr.append([1.0,float(lineArr[0]),float(lineArr[1])])
-            LabelArr.append(int(lineArr[2]))
+            dataMat.append([1.0,float(lineArr[0]), float(lineArr[1])])
+            LabelMat.append(int(lineArr[2]))
 
-    return dataArr,LabelArr
+    return dataMat, LabelMat
+
+def sigmod(X):
+    return 1/(1+exp(-X))
+'''
+def grd():
+'''
+
 
 
 def poltBestFit():
-    import matplotlib.pyplot as plt
-    dataMat ,labelMat = loadFile("testSet.txt")
+
+    dataMat , labelMat = loadFile("testSet.txt")
     dataArr = np.array(dataMat)
+
     n = np.shape(dataArr)[0]
     xcord1 = []
     ycord1 = []
@@ -33,12 +42,13 @@ def poltBestFit():
             xcord2.append(dataArr[i,1]); ycord2.append(dataArr[i,2])
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.scatter(xcord1,ycord1,s=30,c='red',marker ='s')
-    ax.scatter(xcord2,ycord2,s=30,c='green')
-    #x = np.arange(-3.0,3.0,0.1)
+    ax.scatter(xcord1, ycord1, s=30, c='red', marker='s')
+    ax.scatter(xcord2, ycord2, s=30, c='green')
+    x = np.arange(-3.0, 3.0, 0.1)
     #y = (-weights[0] -weights[1]*x)/weights[2]
     #ax.plot(x,y)
-    plt.xlabel('X1');plt.ylabel('Y2')
+    plt.xlabel('X1')
+    plt.ylabel('Y2')
     plt.show()
 
 
