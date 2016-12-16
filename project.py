78d948a3ce667f9a79c950eeb3407399c217bc7c
FireJohnny <FireJohnny@outlook.com>
2016-12-16 10:42:12 +0800

    no

--- project.py
+++ project.py
@@ -17,7 +17,6 @@ def loadFile(fileName):
 
     return dataArr, LabelArr
 
-
 def sigmoid(X):
     return 1/(1+np.exp(-X))
 
@@ -33,8 +32,10 @@ def gradscent(dataArr,LabelArr):
 
     for i in range(Cycles):
         h = sigmoid(dataMat * weights)
+        #print type(h)
         error = LabelMat - h
-        weights = weights + alpha * dataMat.transpose() * error
+        weights = weights + alpha * dataMat.transpose() * error #dataMat.transpose()*error 是梯度
+        print weights
 
     return np.array(weights)
 
@@ -42,11 +43,12 @@ def stoGradAscent0(dataArr,labelArr):
     dataMat = np.array(dataArr)
     #labelMat  = np.mat(labelArr).transpose()
     m, n =  np.shape(dataMat)
-    alpha = 0.001
+    alpha = 0.01
     weights = np.ones(n)
 
     for i in range(m):
-        h = sigmoid(sum(dataMat * weights))
+        h = sigmoid(sum(dataMat[i] * weights))
+        print h
         error = labelArr[i] - h
         weights = weights + alpha * error * dataMat[i]
 
@@ -55,26 +57,28 @@ def stoGradAscent0(dataArr,labelArr):
 def stocGradAscent1(dataMatrix, classLabels, numIter=150):
     #print type(dataMatrix)
     m,n = np.shape(dataMatrix)
-    weights = np.ones(n)   #initialize to all ones
+    weights = np.ones(n)
     for j in range(numIter):
         dataIndex = range(m)
         for i in range(m):
-            alpha = 4/(1.0+j+i)+0.01    #apha decreases with iteration, does not
-            randIndex = int(np.random.uniform(0,len(dataIndex)))#go to 0 because of the constant
+            alpha = 4/(1.0+j+i)+0.01
+            randIndex = int(np.random.uniform(0,len(dataIndex)))
             h = sigmoid(sum(dataMatrix[randIndex]*weights))
             error = classLabels[randIndex] - h
             weights = weights + alpha * error * dataMatrix[randIndex]
             del(dataIndex[randIndex]) #计算完的样本就进行删除就好
     return weights
 
+
 def poltBestFit():
 
     dataArr , labelArr = loadFile("testSet.txt")
     dataArr = np.array(dataArr)
     #weights = gradscent(dataArr,labelArr)
-    #weights = gradscent(dataArr,labelArr)
+    #weights = stoGradAscent0(dataArr,labelArr)
 
-    weights = stocGradAscent1(dataArr,labelArr)
+    #weights = stocGradAscent1(dataArr,labelArr)
+    weights  = conjjugate(dataArr,labelArr)
     n = np.shape(dataArr)[0]
     xcord1 = []
     ycord1 = []
@@ -96,51 +100,10 @@ def poltBestFit():
     plt.ylabel('Y2')
     plt.show()
 
-
 def classifyVector(intX , weights):
     prob =sigmoid(sum(intX * weights))
     if prob >0.5:return 1.0
     else : return 0.0
-
-
-
-
-'''
-def colicTest():
-    frTrain = open("horseColicTraining.txt")
-    frTest = open("horseColicTest.txt")
-    trainingSet = []
-    trainingLabels = []
-    for line in frTrain.readlines():
-        currLine = line.strip().split('\t')
-        lineArr = []
-        for i in range(21):
-            lineArr.append(float(currLine[i]))
-
-        trainingSet.append(lineArr)
-        trainingLabels.append(float(currLine[21]))
-    trainWeights = stoGradAscent0(np.array(trainingSet),trainingLabels,500)
-    errorCount = 0
-    numTestVec = 0.0
-    for line in frTest.readlines():
-        numTestVec += 1.0
-        currLine = line.strip().split('\t')
-        lineArr = []
-        for i in range(21):
-            lineArr.append(float(currLine[i]))
-        if int(classifyVector(np.array(lineArr),trainWeights)) != int(currLine[21]):
-            errorCount +=1
-    errorRate = (float(errorCount)/numTestVec)
-    print errorRate
-    return errorRate
-
-def multiTest():
-    numTests = 10
-    errorSum = 0.0
-    for k in range(numTests):
-        errorSum += colicTest()
-        print "%d   ...   %f"(numTests,errorSum/(float(numTests)))
-'''
 
 
 if __name__ == "__main__":
